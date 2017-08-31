dc1503668aa2b0e94c8cafec9077ebee6288cd3e
tshevchenko271285 <tshevchenko@ro.ru>
2017-08-31 10:24:24 +0300

    send version

--- js/functions.js
+++ js/functions.js
@@ -20,14 +20,38 @@ function addMove(){
 		direction: direction,
 		duration: duration
 	};
+	buildTable();
+	deleteMove();
+}
 
-	$('#tableMove')
-		.append(
-			'<tr><td>'+cntRowTable+
-			'</td><td>'+direction+
-			'</td><td>'+duration+
-			'</td><td>edit, delete </td></tr>'
-			);
+
+function buildTable(){
+	var table = $('#tableMove');
+	var rows = $('[data-row]');
+	if(rows.length > 0) {
+		rows.remove();
+	}
+	for(element in motions){
+		table.append(
+			'<tr data-row='+motions[element]['cntRowTab']+
+			'><td>'+motions[element]['cntRowTable']+
+			'</td><td>'+motions[element]['direction']+
+			'</td><td>'+motions[element]['duration']+
+			'</td><td data-edit="'+motions[element]['cntRowTab']+'"">edit</td>'+
+			'<td data-delete="'+motions[element]['cntRowTab']+'"">delete</td></tr>'
+		);
+	};
+}
+
+
+function deleteMove(){
+	var editBtn = $('[data-delete]');
+	editBtn.each(function(index, element){
+		$(element).on('click', function(){
+			delete motions[index];
+			buildTable();
+		});
+	});
 }
 
 
@@ -76,13 +100,49 @@ function animation(){
 
 }
 
+/*
+**	Defining the border 
+**	of the parent element
+*/
+	function border(obj, param){
+	var result = true;
+
+	var topObj = $(obj)[0]['offsetTop'];
+	var leftObj = $(obj)[0]['offsetLeft'];
+
+	var widthObj = $(obj)[0]['offsetWidth'];
+	var heightObj = $(obj)[0]['offsetHeight'];
+
+	var parent = $(obj).parent();
+
+	var parentLeft = parent[0]['offsetLeft'];
+	var parentRight = parent[0]['offsetWidth'] - parentLeft;
+	var parentTop = parent[0]['offsetTop'];
+	var parentBottom = parent[0]['offsetHeight'] + parentTop - heightObj*2;
+	switch(param) {
+		case 'left':
+			if(leftObj <= parentLeft) 
+			result = false;
+		case 'right':
+			if(leftObj >= parentRight)
+			result = false;
+		case 'top':
+			if(topObj <= parentTop)
+			result = false;
+		case 'bottom':
+			if(topObj >= parentBottom)
+			result = false;
+	}
+	return result;
+
+}
+
 
 /*
 **	Wrapper functions 
 **	for moving a square
 */
 function leftSquare(obj, duration){
-	console.log('x');
 	obj.animate({
 		left: "-=10"
 	}, duration);
@@ -105,48 +165,4 @@ function topSquare(obj, duration){
 
 
 
-function border(obj, param){
 
-	var result = true;
-
-	var topObj = $(obj)[0]['offsetTop'];
-	var leftObj = $(obj)[0]['offsetLeft'];
-
-	var widthObj = $(obj)[0]['offsetWidth'];
-	var heightObj = $(obj)[0]['offsetHeight'];
-
-	var parent = $(obj).parent();
-
-	var parentLeft = parent[0]['offsetLeft'];
-	var parentRight = parent[0]['offsetWidth'] - parentLeft;
-	var parentTop = parent[0]['offsetTop'];
-	var parentBottom = parent[0]['offsetHeight'] + parentTop - heightObj*2;
-	console.log(parentTop);
-	switch(param) {
-		case 'left':
-			if(leftObj <= parentLeft) 
-			result = false;
-		case 'right':
-			if(leftObj >= parentRight)
-			result = false;
-		case 'top':
-			if(topObj <= parentTop)
-			result = false;
-		case 'bottom':
-			if(topObj >= parentBottom)
-			result = false;
-	}
-	return result;
-
-}
-
-
-echo "# justwell" >> README.md
-git init
-git add README.md
-git commit -m "First commit"
-git remote add tima https://github.com/tshevchenko271285/justwell.git
-git push -u tima master
-
-git remote add tima https://github.com/tshevchenko271285/justwell.git
-git push -u tima master
